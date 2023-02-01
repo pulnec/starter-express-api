6856875d1cb9ac7e561987c68cde4939601f6332
Jorge Andrade <andrade091.app@gmail.com>
2023-01-31 21:05:13 -0300

    change file json

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,14 +1,25 @@
 import admin from "firebase-admin";
 import { readFile } from "node:fs/promises";
+import encryptor from "simple-encryptor";
+import * as dotenv from "dotenv";
+dotenv.config();
+const key = process.env.KEY_SALT;
+const encryptGen = encryptor.createEncryptor(key);
 
 export const firebaseInit = async () => {
-  const accountService = await readFile(
-    "./firebase/cambiando-app-firebase-adminsdk-jzyss-a14c10d85c.json",
-    "utf-8"
-  ).then(JSON.parse);
-  admin.initializeApp({
-    credential: admin.credential.cert(accountService),
-  });
+  const accountService = await readFile("./firebase/config.json", "utf-8").then(
+    JSON.parse
+  );
+  const accountConfig = encryptGen.decrypt(accountService.data);
+
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(accountConfig),
+    });
+    console.log("Firebase ON!");
+  } catch {
+    console.log("Firebase OFF!");
+  }
 };
 
 export const sendPushNotification = (title, body) => {
